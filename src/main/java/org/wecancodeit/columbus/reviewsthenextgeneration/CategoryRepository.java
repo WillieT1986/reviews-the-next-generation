c68aafea559804c96666300aac604f4ecc6e1c83
WillieT1986 <WarMad2@Hotmail.com>
2018-03-16 14:12:29 -0400

    Added extends CrudRepository<Category, Long>

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/CategoryRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/CategoryRepository.java
@@ -1,5 +1,7 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
-public interface CategoryRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
 
 }
