4881d1f3e991238b43663c5f0321d540f4da46f6
WillieT1986 <WarMad2@Hotmail.com>
2018-03-16 16:44:18 -0400

    Created ReviewsPopulator implements CommandLineRunner Class with @Override/public void run(String... args) throws Exception {}

---
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewsPopulator.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.columbus.reviewsthenextgeneration;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class ReviewsPopulator implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
