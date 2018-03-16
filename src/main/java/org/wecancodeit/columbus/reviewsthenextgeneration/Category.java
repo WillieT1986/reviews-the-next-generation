ecd6d20c79fa595be2075a72c0ba6646713f9d58
WillieT1986 <WarMad2@Hotmail.com>
2018-03-16 14:14:35 -0400

    Added @Id @GeneratedValue private long id;

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/Category.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/Category.java
@@ -1,6 +1,13 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Category {
+
+	@Id
+	@GeneratedValue
+	private long id;
 
 	public Category() {
 
