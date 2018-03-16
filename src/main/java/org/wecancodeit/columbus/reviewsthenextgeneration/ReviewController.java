11b88279e1704a51ce863545f91f1e660767325d
WillieT1986 <WarMad2@Hotmail.com>
2018-03-16 16:51:21 -0400

    Added three @Resource annotations. One for ReviewRepository, CategoryRepository and TagRepository. Great place to stop since it is the end of class.

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewController.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewController.java
@@ -1,8 +1,19 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ReviewController {
+
+	@Resource
+	private ReviewRepository reviewRepo;
+
+	@Resource
+	private CategoryRepository categoryRepo;
+
+	@Resource
+	private TagRepository tagRepo;
 
 }
