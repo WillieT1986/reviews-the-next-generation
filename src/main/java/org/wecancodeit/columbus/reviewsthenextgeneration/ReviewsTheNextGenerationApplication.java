fc88eb1bd9af342b1e7ba9be6eb5c940f40809ed
WillieT1986 <WarMad2@Hotmail.com>
2018-03-16 10:34:56 -0400

    Initial Commit

---
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewsTheNextGenerationApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.columbus.reviewsthenextgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsTheNextGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsTheNextGenerationApplication.class, args);
+	}
+}
