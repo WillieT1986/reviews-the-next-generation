ff4b546e092064d4ccba79f9c19c566d30eb6d20
WillieT1986 <WarMad2@Hotmail.com>
2018-03-16 13:13:49 -0400

    Created getId() through testing that returns an id. @Id, @GeneratedValue was added to private long id;

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/Review.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/Review.java
@@ -1,12 +1,22 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Review {
 
+	@Id
+	@GeneratedValue
+	private long id;
+
 	public Review() {
 
+	}
+
+	public long getId() {
+		return id;
 	}
 
 }
