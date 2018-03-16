41e743a045799b65f2c3a51953dda7992f868904
WillieT1986 <WarMad2@Hotmail.com>
2018-03-16 13:19:46 -0400

    Deleted an empy line in Review Class. Added CrudRepository<Review, Long> to ReviewRepository Interface

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewRepository.java
@@ -1,5 +1,7 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
-public interface ReviewRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewRepository extends CrudRepository<Review, Long> {
 
 }
