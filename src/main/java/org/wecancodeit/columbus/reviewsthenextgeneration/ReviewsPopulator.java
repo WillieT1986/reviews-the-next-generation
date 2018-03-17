c13a5117bdd72eec6b291864b24a115bbede666c
WillieT1986 <WarMad2@Hotmail.com>
2018-03-17 02:56:32 -0400

    Added description

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewsPopulator.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewsPopulator.java
@@ -105,8 +105,12 @@ public class ReviewsPopulator implements CommandLineRunner {
 				"This story is about a boy named Kóinzell who's much older than he seems. Once know to be apart of a group of Hero's who ended up being betrayed. Kóinzell, seeks vengeance against the Hero's"
 						+ " who betrayed him and his comrades and made them out to be traitors. Kóinzell is not just an object of fear but also of hope to those around him.",
 				tag1, tag2, tag6, tag7, tag18));
-		reviewRepo.save(new Review(manga, "The Heroic Legend of Arslan", "./images/arslan.jpg", "Description2", tag1,
-				tag2, tag5, tag7, tag9, tag16, tag19));
+		reviewRepo.save(new Review(manga, "The Heroic Legend of Arslan", "./images/arslan.jpg",
+				"Betrayed from within and overwhelmed by dark sorcery, the valiant armies of Pars are overrun by the warrior-fanatics of Lusitania. Escaping the destruction of his homeland, "
+						+ "the heroic Prince Arslan begins to assemble a group of five, extraordinary allies—a priestess, an exiled warrior, a mischievous minstrel, an insightful artist and his"
+						+ " attendant—to aid him in his fight against the Lusitanian invaders. Together, they must build an army and defeat 300,000 trained soldiers—including the mysterious Lord"
+						+ " Silver Mask—who has also laid claim to the throne of Pars. - Pulled from MyAnimeList.com that had the most simplist for this Manga",
+				tag1, tag2, tag5, tag7, tag9, tag16, tag19));
 		reviewRepo.save(new Review(manga, "manga3", "./images/image3.png", "Description3", tag1));
 		// End of Manga Review
 	}
