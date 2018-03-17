1a93e8ecd5b019f12a377d51abc35ac50059d5e6
WillieT1986 <WarMad2@Hotmail.com>
2018-03-17 14:28:59 -0400

    Created getComment {return comment}

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/Comment.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/Comment.java
@@ -5,8 +5,13 @@ import javax.persistence.Entity;
 @Entity
 public class Comment {
 
+	private String comment;
+
 	public Comment(String comment) {
 		this.comment = comment;
 	}
 
+	public String getComment() {
+		return comment;
+	}
 }
