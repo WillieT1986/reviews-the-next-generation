8f7572facc574a580979336630b6769ec84851ac
WillieT1986 <WarMad2@Hotmail.com>
2018-03-17 01:25:04 -0400

    Copied the 17 created tags from Reviews Site 2 to save time. Ran tests, tests still pass.

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewsPopulator.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewsPopulator.java
@@ -21,6 +21,25 @@ public class ReviewsPopulator implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Category anime = createCategory("Anime Reviews");
 		Category manga = createCategory("Mange Reviews");
+
+		Tag tag1 = createTag("Action");
+		Tag tag2 = createTag("Adventure");
+		Tag tag3 = createTag("Comedy");
+		Tag tag4 = createTag("Demons");
+		Tag tag5 = createTag("Drama");
+		Tag tag6 = createTag("Ecchi");
+		Tag tag7 = createTag("Fantasy");
+		Tag tag8 = createTag("Game");
+		Tag tag9 = createTag("Historical");
+		Tag tag10 = createTag("Magic");
+		Tag tag11 = createTag("Martial Arts");
+		Tag tag12 = createTag("Military");
+		Tag tag13 = createTag("Romance");
+		Tag tag14 = createTag("Samurai");
+		Tag tag15 = createTag("School");
+		Tag tag16 = createTag("Shounen");
+		Tag tag17 = createTag("Super Powers");
+
 	}
 
 	private Category createCategory(String catName) {
