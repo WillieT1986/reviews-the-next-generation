4b86948825245753a40da3a460fa5da90a8c93a1
WillieT1986 <WarMad2@Hotmail.com>
2018-03-17 15:10:13 -0400

    @Resource, CommentRepository commentRepo; added to CommentController

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/CommentController.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/CommentController.java
@@ -1,8 +1,12 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CommentController {
+	@Resource
+	CommentRepository commentRepo;
 
 }
