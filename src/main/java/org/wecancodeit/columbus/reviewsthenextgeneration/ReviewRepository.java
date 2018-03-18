eaf8c31cb5b79ad293fa3e529870f60a54497162
WillieT1986 <WarMad2@Hotmail.com>
2018-03-18 14:04:44 -0400

    Program crashes. Error: CommentRepository.findByName(java.lang.String)! No property name found for type Comment!

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewRepository.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/ReviewRepository.java
@@ -3,5 +3,5 @@ package org.wecancodeit.columbus.reviewsthenextgeneration;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewRepository extends CrudRepository<Review, Long> {
-	// Review findByName(String reviewTitle);
+	Review findByName(String reviewTitle);
 }
