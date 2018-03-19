825e80c7053091c066a8428857e99e1c7fefad7c
WillieT1986 <WarMad2@Hotmail.com>
2018-03-19 02:22:22 -0400

    Started an if() {} statement

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -4,6 +4,10 @@ const addTag = document.querySelector('.add-tag')
 addTag.addEventListener('click', function() {
 	const addTagInput = document.querySelector('addTag')
 	const addTag = addTagInput.value
-	
+
+	if () {
+		
+	}
+
 
 })
