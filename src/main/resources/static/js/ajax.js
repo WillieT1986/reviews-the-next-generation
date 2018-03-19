730fcf043a735105b997ee0c70e34740231b9ad7
WillieT1986 <WarMad2@Hotmail.com>
2018-03-19 02:18:28 -0400

    const addTag = addTagInput.value

--- src/main/resources/static/js/ajax.js
+++ src/main/resources/static/js/ajax.js
@@ -3,5 +3,7 @@ const addTag = document.querySelector('.add-tag')
 
 addTag.addEventListener('click', function() {
 	const addTagInput = document.querySelector('addTag')
+	const addTag = addTagInput.value
+	
 
 })
