a88a2ef8794f7a0052b8146a28f670ef3400796c
WillieT1986 <WarMad2@Hotmail.com>
2018-03-19 03:07:54 -0400

    Added a bunch of code for adding a Tag.

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/TagRestController.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/TagRestController.java
@@ -2,6 +2,8 @@ package org.wecancodeit.columbus.reviewsthenextgeneration;
 
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,23 @@ public class TagRestController {
 
 	@Resource
 	TagRepository tagRepo;
+
+	@RequestMapping("/tag/{tagId}/addtag/{tagName}")
+	public Tag addTagFromTags(@PathVariable Long tagId, @PathVariable String tagName) {
+		Tag tag = tagRepo.findOne(tagId);
+		Tag tag1 = tagRepo.findByNameIgnoreCase(tagName);
+
+		if (tag1 == null) {
+			tag1 = new Tag(tagName);
+			tag1.save(tag1);
+		}
+		String result = tag.addTag(tag1);
+		tagRepo.save(tag);
+		if(result.equals("added")) {
+			return tag1;
+		} else {
+			retun null;
+		}
+	}
+
 }
