644122de0735087c18274384e582a23f6abb647f
WillieT1986 <WarMad2@Hotmail.com>
2018-03-19 02:53:32 -0400

    Added @Resource for ReviewRepository and TagRepository

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/TagRestController.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/TagRestController.java
@@ -1,8 +1,14 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TagRestController {
+	@Resource
+	ReviewRepository reviewRepo;
 
+	@Resource
+	TagRepository tagRepo;
 }
