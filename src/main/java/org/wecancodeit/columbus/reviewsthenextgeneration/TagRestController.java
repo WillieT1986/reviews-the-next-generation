d1e86260c9fe31f396ef55fc690e50cd4c7c70be
WillieT1986 <WarMad2@Hotmail.com>
2018-03-19 02:51:42 -0400

    Added @RestController

--- src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/TagRestController.java
+++ src/main/java/org/wecancodeit/columbus/reviewsthenextgeneration/TagRestController.java
@@ -1,5 +1,8 @@
 package org.wecancodeit.columbus.reviewsthenextgeneration;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TagRestController {
 
 }
